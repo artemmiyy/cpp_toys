c3fe80cfd4b205bc3a856becc6108fb04bdd6ed6
Artemii Polshcha <artempolshcha@gmail.com>
2024-03-17 13:56:41 -0400

    added a rotate function and playfield

--- Tetris/source.cpp
+++ Tetris/source.cpp
@@ -3,19 +3,24 @@
 // there are 7 tetromino pieces
 std::wstring tetromino[7];
 
-int rotation(int px, int py, int r) {
+int playfield_w = 12;
+int playfield_h = 18;
+
+// for storing elements of the field
+unsigned char *playfield = nullptr;
+
+int rotate(int x_coord, int y_coord, int r) {
 	switch (r % 4)
 	{
-		case 0: return py * 4 + px;        // 0 deg rotation
-		case 1: return 12 + py - (px * 4); // 90 deg rotation
-		case 2: return 15 - (py * 4) - px; // 180 deg rotation
-		case 3: return 3 - py + (px * 4);  // 270 deg rotation
+		case 0: return y_coord * 4 + x_coord;        // 0 deg rotation
+		case 1: return 12 + y_coord - (x_coord * 4); // 90 deg rotation
+		case 2: return 15 - (y_coord * 4) - x_coord; // 180 deg rotation
+		case 3: return 3 - y_coord + (x_coord * 4);  // 270 deg rotation
 	}
 	return 0;
 }
 
 int main() {
-	
 	// creating shapes
 	tetromino[0].append(L"..X.");
 	tetromino[0].append(L"..X.");
@@ -52,5 +57,12 @@ int main() {
 	tetromino[6].append(L".X..");
 	tetromino[6].append(L".X..");
 
+	// initalize array for the playfield
+	playfield = new unsigned char[playfield_w * playfield_h];
+	// playfield boundary
+	for (int x = 0; x < playfield_w; x++)
+		for (int y = 0; y < playfield_h; y++)
+			playfield[y * playfield_w + x] = (x == 0 || x == playfield_w - 1
+			|| y == playfield_h - 1) ? 8 : 0; // 8 represents border
 	return 0;
 }
